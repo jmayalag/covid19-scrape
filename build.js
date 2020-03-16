7baadc21da95291a7e21f6a57505a228e9b94183
Jordan Ayala <jmayala@protonmail.com>
2020-03-15 22:25:30 -0300

    remove gh-pages

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ const updateCases = async () => {
   const cases = await fetchCases();
   const json = JSON.stringify({ ...cases });
   console.log(json);
-  fs.writeFileSync("public/cases.json", json);
+  fs.writeFileSync("cases.json", json);
 };
 
 updateCases();
